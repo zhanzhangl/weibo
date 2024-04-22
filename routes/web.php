cbccc7af79f1a7d9ed66b1aca483cfbf58447d08
vagrant <vagrant@homestead>
2024-04-22 14:58:07 +0000

    命名路由

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,12 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::get('/', 'StaticPagesController@home');
-Route::get('/help', 'StaticPagesController@help');
-Route::get('/about', 'StaticPagesController@about');
+// Route::get('/', 'StaticPagesController@home');
+// Route::get('/help', 'StaticPagesController@help');
+// Route::get('/help', 'StaticPagesController@help')->name('help');
+// Route::get('/about', 'StaticPagesController@about');
+
+Route::get('/', 'StaticPagesController@home')->name('home');
+Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::get('/about', 'StaticPagesController@about')->name('about');
+
