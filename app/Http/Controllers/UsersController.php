0016575ad9a86c1ddf3195b0bfa8d3464a496060
vagrant <vagrant@homestead>
2024-04-22 17:36:57 +0000

    用户注册表单

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -1,14 +1,19 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
+    // use App\Models\User;
     //
     public function create()
     {
         return view('users.create');
+    }
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function gravatar($size = '100')
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "https://cravatar.cn/avatar/$hash?s=$size";
+    }
 }
