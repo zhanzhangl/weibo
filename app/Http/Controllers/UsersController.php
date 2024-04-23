9b947bc6ed4ec6a905d35e5cf6d27ae95ad9596f
vagrant <vagrant@homestead>
2024-04-23 05:15:31 +0000

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -20,11 +20,22 @@ class UsersController extends Controller
     //
     public function store(Request $request)
     {
+        // Step 1 数据过滤
         $this->validate($request, [
             'name' => 'required|unique:users|max:50',
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
-        return;
+        // Step 2 输入数据
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+
+        return redirect()->route('users.show', [$user]);
     }
+
 }
