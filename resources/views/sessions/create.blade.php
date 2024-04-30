0f60a26a0aec3059f951f3c96373d94cf06c7e8c
vagrant <vagrant@homestead>
2024-04-30 15:53:18 +0000

    开启登录记住我

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -13,14 +13,21 @@
       <form method="POST" action="{{ route('login') }}">
           {{ csrf_field() }}
 
-          <div class="mb-3">
+          <div class="form-group">
             <label for="email">邮箱：</label>
             <input type="text" name="email" class="form-control" value="{{ old('email') }}">
           </div>
 
-          <div class="mb-3">
+          <div class="form-group">
             <label for="password">密码：</label>
             <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+          </div>
+
+          <div class="form-group">
+            <div class="form-check">
+              <input type="checkbox" class="form-check-input" name="remember" id="exampleCheck1">
+              <label class="form-check-label" for="exampleCheck1">记住我</label>
+            </div>
           </div>
 
           <button type="submit" class="btn btn-primary">登录</button>
