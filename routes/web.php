5f7faa7339b933e41237b949f7c0e47c12ad5694
zhanzhangl <zhanzhangl2023@163.com>
2024-05-03 10:28:43 +0000

    用户关注和取消关注按钮

--- routes/web.php
+++ routes/web.php
@@ -54,3 +54,7 @@ Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy'
 Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
 // 获取用户粉丝列表
 Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
+// 获取用户粉丝列表
+Route::post('/users/followers/{user}', 'FollowersController@store')->name('followers.store');
+// 取消用户粉丝列表
+Route::delete('/users/followers/{user}', 'FollowersController@destroy')->name('followers.destroy');
